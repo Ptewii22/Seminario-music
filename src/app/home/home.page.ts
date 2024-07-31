e44b3bef65aab70d26cbd4352cd9b7186e3d1bcf
Luis Angel <luicamargo80@gmail.com>
2024-07-31 00:50:59 -0500

    Footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
 import { MusicService } from '../services/music.service';
+import { ModalController } from '@ionic/angular';
+import { SongModalPage } from '../song-modal/song-modal.page';
 
 
 @Component({
@@ -13,19 +15,55 @@ export class HomePage implements OnInit {
  
   artistsJson: any;
   artists: any;
-  constructor(private router: Router, private storange: Storage, private musicService: MusicService ) { }
+  song = {
+    name : '',
+    playing: false,
+    preview_url: ''
+  }
+  
+  constructor(private router: Router, private storange: Storage, private musicService: MusicService, private modalController: ModalController) { }
 
   ngOnInit() {
     this.artists =this.musicService.getArtistsJson().artists;
     console.log("Json",this.artists)
-    this.musicService.getArtists().subscribe((data: any) =>{
+   /* this.musicService.getArtists().subscribe((data: any) =>{
       this.artists =data
       console.log(this.artists)
 
 
     })
+*/
 
+this.musicService.getArtists().then(data =>{
+  this.artists = data;
+console.log(data)
+
+
+})
   }
 
 
+ async showSongs(artists: any){
+    console.log(artists)
+    const songs = await this.musicService.getArtistTracks(artists.id);
+    const modal = await  this.modalController.create(
+
+      {
+        component: SongModalPage,
+        componentProps: {
+
+          name: artists.name,
+          id: artists.id,
+          songs: songs
+        }
+
+      }
+    );
+    modal.onDidDismiss().then(dataReturned  =>{
+      this.song = dataReturned.data;
+    })
+    
+    modal.present();
+
+}
 }
